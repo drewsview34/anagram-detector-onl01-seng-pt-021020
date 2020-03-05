cc0a34c67e76b90120e6e1d96ea86c42d14d20be
Andrew Hinojosa <drews_view@hotmail.com>
2020-03-05 20:17:35 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,14 @@ class Anagram
   
   attr_accessor :word
   
-  @words = []
+  words = []
   
   def initialize(word)
     @word = word
   end
   
-  def self.match(@words)
-    @words.each do |each_word|
+  def self.match(words)
+    words.each do |each_word|
       each_word.split("").sort.eql?(word.split("").sort)
     end 
   end
